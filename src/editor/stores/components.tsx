1416f81e4d405f2e7592cba78b6a8545283aea60
weichen29 <weichen29@iflytek.com>
2025-03-26 20:03:26 +0800

    feta: initialize project with React, TypeScript, and Vite setup; add basic components and styles

---
+++ src/editor/stores/components.tsx
@@ -0,0 +1,82 @@
+import {create} from "zustand/react";
+
+interface Component {
+  id: number,
+  name: string
+  props: unknown,
+  desc?: string,
+  children?: Component[],
+  parentId?: number,
+}
+
+interface State {
+  components: Component[]
+}
+
+interface Action {
+  addComponent: (component: Component, parentId: number) => void,
+  deleteComponent: (componentId: number) => void;
+  updateComponentProps: (componentId: number, props: unknown) => void;
+}
+
+export const useComponents = create<State & Action>(
+  (set, get) => ({
+    components: [
+      {
+        id: 1,
+        name: 'Page',
+        props: {},
+        desc: '页面',
+      }
+    ],
+    addComponent: (component, parentId) => set((state) => {
+      if (parentId) {
+        const parentComponent = getComponentById(parentId, state.components)
+        if (parentComponent) {
+          if (parentComponent.children) {
+            parentComponent.children.push(component)
+          } else {
+            parentComponent.children = [component]
+          }
+        }
+        return {components: [...state.components]}
+      }
+      return {components: [...state.components, component]}
+    }),
+    deleteComponent: () => {
+
+    },
+    updateComponentProps: (componentId) => {
+      if (!componentId) {
+        return
+      }
+      const component = getComponentById(componentId, get().components)
+      if (component?.parentId) {
+        const parentComponent = getComponentById(component.parentId, get().components)
+        if (parentComponent) {
+          parentComponent.children = parentComponent.children?.filter(
+            item => item.id !== +componentId
+          )
+          set({components: [...get().components]})
+        }
+      }
+    }
+  }))
+
+export function getComponentById(id: number, components: Component[]): Component | null {
+  if (!id) {
+    return null
+  }
+  for (const component of components) {
+    if (component.id === id) {
+      return component
+    }
+    if (component.children && component.children.length > 0) {
+      const result = getComponentById(component.id, component.children)
+      if (result !== null) {
+        return result
+      }
+    }
+  }
+  return null
+}
